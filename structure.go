3b7d73550823559545a90d8a3933e83c3e55e5e5
Alexandre Leitão <alexandre.leitao@grupofullsix.pt>
2023-02-08 12:30:01 +0000

    INT: Saving Code

---
+++ structure.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+type structure struct {
+	name     string
+	isRoot   bool
+	fileInfo os.FileInfo
+	children []structure
+	parent   *structure
+}
+
+func createDummyStructure() structure {
+	mainStructure := structure{
+		name:     "2023",
+		children: []structure{},
+	}
+
+	for i := 1; i <= 12; i++ {
+		mainStructure.children = append(mainStructure.children, structure{name: strconv.Itoa(i)})
+	}
+	return mainStructure
+}
+
+func (s structure) makeYearFolderStructure(year string) {
+	root := s.getRoot()
+
+}
+
+func (s structure) makeMonthFolderStructure(month string) {
+
+}
+
+func (s structure) getRoot() structure {
+	if s.isRoot {
+		return s
+	}
+
+	tempStructure := *s.parent
+
+	for tempStructure.isRoot {
+		if tempStructure.isRoot {
+			return tempStructure
+		}
+		tempStructure = *tempStructure.parent
+	}
+
+	return tempStructure
+}
